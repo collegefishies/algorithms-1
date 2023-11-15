7a6ef51668e7a0a6ae1237d0a24baf3725e2d5c6
Enrique Mendez <enrique.phys@gmail.com>
2023-11-15 14:02:52 -0800

    Add API skeleton.

---
+++ interview-questions/solutions/4-ThreeSum/BinarySearch.java
@@ -0,0 +1,9 @@
+public class BinarySearch {
+    public static int left(int[] a, int val) {
+        return -1;
+    }
+
+    public static int right(int[] a, int val) {
+        return -1;
+    }
+}
