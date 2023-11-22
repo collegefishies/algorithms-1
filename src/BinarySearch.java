f5e1472d1ebb8bc958661661ffbae7914efbe4a5
Enrique Mendez <enrique.phys@gmail.com>
2023-11-22 14:11:25 -0800

    Wrote working binary search from first principles on the RIGHT! Milestone!

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -28,7 +28,7 @@ public class BinarySearch {
         while (left <= right) {
             // assert if `val` is in the array, it must be within (left, right).
             int mid = left + (right - left) / 2;
-            StdOut.printf("%-5s: %5d\n%-5s: %5d\n%-5s: %5d\n","left", left, "mid", mid, "right", right);
+            StdOut.printf("%-5s: %5d\n%-5s: %5d\n%-5s: %5d\n", "left", left, "mid", mid, "right", right);
             StdOut.println(Arrays.toString(Arrays.copyOfRange(a, left, right + 1)));
             if (val < a[mid])
                 // assert within(left, right) and val < mid;
@@ -56,39 +56,36 @@ public class BinarySearch {
         }
 
         int mid = left + (right - left) / 2;
-        StdOut.printf("%-5s: %5d\n%-5s: %5d\n%-5s: %5d\n","left", left, "mid", mid, "right", right);
+        StdOut.printf("%-5s: %5d\n%-5s: %5d\n%-5s: %5d\n", "left", left, "mid", mid, "right", right);
         StdOut.println(Arrays.toString(Arrays.copyOfRange(a, left, right + 1)));
 
         return -1;
     }
 
     public static int right(int[] a, int val) {
-        if (a.length == 0) {
-            return -1;
-        }
+        int left = 0;
+        int right = a.length - 1;
 
-        int low = 0;
-        int high = a.length - 1;
-        int mid = (high - low) / 2 + low;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        while (low < high) {
-            if (a[mid] > val) {
-                high = mid - 1;
-            } else if (a[mid] < val) {
-                low = mid + 1;
-            } else {
-                low = mid + 1;
+            if (val < a[mid])
+                right = mid - 1;
+            else if (val > a[mid])
+                left = mid + 1;
+            else {
+                assert a[mid] == val;
+                if (mid == a.length - 1) {
+                    return mid;
+                }
+
+                if (a[mid + 1] == val) {
+                    left = mid + 1;
+                } else {
+                    return mid;
+                }
             }
-
-            mid = (high - low) / 2 + low;
         }
-
-        if (a[high] == val) {
-            return high;
-        } else if (a[high - 1] == val) {
-            return high - 1;
-        }
-
         return -1;
     }
 }
