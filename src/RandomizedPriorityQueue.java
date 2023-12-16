558f6f29668e2a92e658fbbd9f7a6b026c1d11a5
Enrique Mendez <enrique.phys@gmail.com>
2023-12-16 12:19:49 -0800

    Develop RandomizedPriorityQueue. Has all API of MinPQ, plus the ability to randomly sample as well as remove, and still maintain heap ordering.

--- src/RandomizedPriorityQueue.java
+++ src/RandomizedPriorityQueue.java
@@ -8,7 +8,7 @@ import java.util.NoSuchElementException;
 Describe how to add the methods `sample()` and `delRandom()` to our binary heap implementation. The two methods return a key that is chosen uniformly at random among the remaining keys, with the latter method also removing that key. The `sample()` method should take constant time; the `delRandom()` method should take logarithmic time. Do not worry about resizing the underlying array.
 */
 public class RandomizedPriorityQueue<T> {
-    private int N = 1;
+    private int index = 1;// (non -inclusive) Array goes from [1, index)
     private T[] heap;
 
     // api
@@ -17,9 +17,9 @@ public class RandomizedPriorityQueue<T> {
     }
 
     public void insert(T x) {
-        heap[N] = x;
-        swim(N);
-        N++;
+        heap[index] = x;
+        swim(index);
+        index++;
     }
 
     public T min() {
@@ -27,30 +27,34 @@ public class RandomizedPriorityQueue<T> {
     }
 
     public T delMin() {
-        if (N == 1) throw new NoSuchElementException("Queue is empty.");
+        if (size() == 0) throw new NoSuchElementException("Queue is empty.");
         T x = (T) heap[1];
-        swap(heap, 1, N - 1);
-        heap[N - 1] = null;
-        N--;
+        swap(heap, 1, index - 1);
+        heap[index - 1] = null;
+        index--;
         sink(1);
         return x;
     }
 
     public T sample() {
-        int i = StdRandom.uniformInt(1, N);
+        if (size() == 0) throw new NoSuchElementException("No elements to sample from");
+        int i = StdRandom.uniformInt(1, index);
         return heap[i];
     }
 
     public T delRandom() {
-        int i = StdRandom.uniformInt(1, N);
+        if (size() == 0) throw new NoSuchElementException("No elements to sample from and delete.");
+        int i = StdRandom.uniformInt(1, index);
         T x = heap[i];
-        swap(heap, i, N);
-        heap[N--] = null;
+        swap(heap, i, index - 1);
+        heap[index - 1] = null;
+        index--;
+        sink(i);
         return x;
     }
 
     public int size() {
-        return N - 1;
+        return index - 1;
     }
 
     public boolean isEmpty() {
@@ -72,11 +76,11 @@ public class RandomizedPriorityQueue<T> {
     }
 
     private void sink(int i) {
-        while (i < N) {
+        while (i < index) {
             // have no children
-            if (2*i >= N && 2*i + 1 >= N) break;
+            if (2*i >= index && 2*i + 1 >= index) break;
             // have one child
-            else if (2*i+1 >= N) {
+            else if (2*i+1 >= index) {
                 if(greater(i, 2*i))
                     swap(heap, i, 2*i);
                 break;
@@ -105,16 +109,36 @@ public class RandomizedPriorityQueue<T> {
 
     // test
     public static void main(String[] args) {
-        int N = 20;
+        int N = 30;
         int[] a = new int[N];
         for (int i = 0; i < N; a[i++] = i) ;
-        StdRandom.setSeed(42);
+//        StdRandom.setSeed(42);
         RandomizedPriorityQueue<Integer> rpq = new RandomizedPriorityQueue<>(2 * N);
 
         //Check min-priority queue feature.
         StdRandom.shuffle(a);
         for (int x : a) rpq.insert(x);
+        //Print out to ensure it acts like a priority queue
+        StdOut.println("Printing from Priority Queue");
         while (!rpq.isEmpty()) StdOut.println(rpq.delMin());
+        //print samples
+        for (int x : a) rpq.insert(x);
+        int trials = 1000000;
+        double[] counts = new double[N+1];
+        for (int i = 0; i < trials; i++) {
+            counts[rpq.sample()] += 1;
+        }
+        StdOut.println("Relative Frequencies");
+        for (int i = 1; i <= N; i++) {
+            StdOut.println(counts[i]/trials);
+        }
+
+        StdOut.println("Sample randomly from Priority Queue");
+        N /= 2;
+        while ( N-- > 0) StdOut.println(rpq.delRandom());
+        StdOut.println("Print minimums from Priority Queue");
+        while (!rpq.isEmpty()) StdOut.println(rpq.delMin());
+
 
     }
 }
